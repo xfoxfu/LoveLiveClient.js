0d7966d48eec5f29311e87d86a91d441579e22b7
小傅Fox <coderfox.fu@gmail.com>
2016-04-08 17:21:10 +0800

    feature(index): client.buildUpConfigOpts

--- config.ts
+++ config.ts
@@ -6,8 +6,28 @@ try {
 } catch (ex) {
   config_json = require("./config.sample.json");
 }
+
+interface IHeaders {
+  "application-id": string;
+  "accept": string;
+  "time-zone": string;
+  "api-model": string;
+  "client-version": string;
+  "host": string;
+  "os": string;
+  "accept-encoding": string;
+  "debug": string;
+  "region": string;
+  "bundle-version": string;
+  "os-version": string;
+  "platform-type": string;
+  "authorize": string;
+  "content-type"?: string;
+  "x-message-code"?: string;
+}
 let config = {
-  hmac_key: <string>config_json["hmac_key"]
+  hmac_key: <string>config_json["hmac_key"],
+  headers: <IHeaders>config_json["headers"]
 };
 
 export = config;
--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@
 
 import crypto = require("crypto");
 import config = require("./config");
+import request = require("request-promise");
+import * as Request from "request";
+const utils = require("utility");
+
 export = class {
   calculateHash(data: string | Object): string {
     let plainText: string;
@@ -11,5 +15,21 @@ export = class {
       plainText = JSON.stringify(data);
     }
     return crypto.createHmac("sha1", config.hmac_key).update(plainText).digest().toString("hex");
+  }
+  buildUpRequestOpt(module: string, api: string, nonce: string, data?: any, token?: string): Request.Options {
+    let result: Request.Options = {
+      uri: `http://prod-jp.lovelive.ge.klabgames.net/main.php/${module}/${api}`,
+      method: "POST",
+      headers: config.headers,
+      json: true
+    };
+    result.headers["authorize"] = `consumerKey=lovelive_test&timeStamp=${utils.timestamp()}&version=1.1&nonce=${nonce}`;
+    if (token) {
+      result.headers["authorize"] = `${result.headers["authorize"]}&token=${token}`;
+    }
+    if (data) {
+      result.formData = { request_data: JSON.stringify(data) };
+    }
+    return result;
   }
 }
