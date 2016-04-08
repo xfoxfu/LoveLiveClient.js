81381eef634e49feec9f9a92cccc6d2aee35ec52
小傅Fox <coderfox.fu@gmail.com>
2016-04-08 16:40:03 +0800

    feature(config): config

--- config.ts
+++ config.ts
@@ -1,7 +1,13 @@
 /// <reference path="./references.d.ts" />
 
+let config_json: any;
+try {
+  config_json = require("./config.json");
+} catch (ex) {
+  config_json = require("./config.sample.json");
+}
 let config = {
-  hmac_key: ""
+  hmac_key: <string>config_json["hmac_key"]
 };
 
 export = config;
